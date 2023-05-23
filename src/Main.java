f1e40f67c2a77b1b4b0485a64c834c74fe8e8736
Oleksandr Kamenskyi <okamenskyi@MacBook-Pro-von-Oleksandr.local>
2023-05-23 10:44:36 +0200

    Adeded Datenbank Class. (Not ready)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 
 import Calendar.Termin;
+import IOManager.Database;
 
 public class Main {
     public static void main(String[] args) {
         testDates();
+        //testDB();
     }
 
-    public static void testDates(){
+    public static void testDates() {
         Termin eintaegigerTermin = new Termin("Ein Tag","MT",true,"2022-12-21", "2022-12-21", "12:00","13:00");
         System.out.println(eintaegigerTermin.title);
         System.out.println(eintaegigerTermin.start);
@@ -16,5 +18,9 @@ public class Main {
         System.out.println(mehrtaegigerTermin.title);
         System.out.println(mehrtaegigerTermin.start);
         System.out.println(mehrtaegigerTermin.end);
+    }
+    public static void testDB() {
+        Database db = new Database();
+
     }
 }
