54884659eefff5656db779f35bf06a80fce44f12
PhilippV <philipp.voss@hotmail.de>
2023-07-16 22:14:31 +0200

    Main-Methode in PanelMain erstellt nun 50 zufällige Termine zum Testen des UIs

--- src/GUI/PanelMain.java
+++ src/GUI/PanelMain.java
@@ -7,6 +7,9 @@ import GUI.monthView.MonthView;
 
 import javax.swing.*;
 import java.awt.*;
+import java.time.YearMonth;
+import java.util.Random;
+
 /**
  * Diese Klasse repräsentiert das Hauptpanel der Anwendung.
  *
@@ -24,13 +27,14 @@ public class PanelMain extends JPanel {
     private MonthView monthView;
 
     public PanelMain(){
+        YearMonth currentYearMonth = YearMonth.now();
         viewManager = new CalendarViewManager();
         mainFrame = new JFrame("Main Panel");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setOpaque(false);
         setLayout(new BorderLayout());
         panelChange = new PanelChange(viewManager);
-        monthView = new MonthView(2023, 6);
+        monthView = new MonthView(currentYearMonth.getYear(), currentYearMonth.getMonthValue());
 
         tabbedPane = new JTabbedPane();
         tabbedPane.addTab("Monatsansicht", monthView);
@@ -47,10 +51,36 @@ public class PanelMain extends JPanel {
     }
 
     public static void main(String[] args){
-        GUI.PanelMain panelMain = new GUI.PanelMain();
-        Termin termin1 = new Termin("Terminname 1", "Typ 1", false, "2023-06-03", "2023-06-03", "10:00", "11:30");
-        Termin termin2 = new Termin("Terminname 2", "Typ 2", false, "2023-06-01", "2023-06-01", "13:00", "14:30");
-        panelMain.getMonthView().addAppointment(termin1);
-        panelMain.getMonthView().addAppointment(termin2);
+        PanelMain panelMain = new GUI.PanelMain();
+
+        YearMonth currentYearMonth = YearMonth.now();
+
+
+        Random random = new Random();
+
+        for (int i = 1; i <= 50; i++) {
+            // Zufälliger Tag
+            int day = random.nextInt(currentYearMonth.lengthOfMonth()) + 1;
+            // Zufällige Startzeit
+            int startHour = random.nextInt(13) + 6;
+            int startMinute = (random.nextInt(4) * 15)%60;
+            // Jeder Termin dauert 2 genau Stunden
+            int endHour = (startHour + 2) % 24;
+            int endMinute = startMinute;
+
+            // Erstellen von Termin mit zufälligen Tag und Startzeit
+            Termin termin = new Termin(
+                    "Terminname " + i,
+                    "Typ " + i,
+                    false,
+                    String.format("%d-%02d-%02d", currentYearMonth.getYear(), currentYearMonth.getMonthValue(), day),
+                    String.format("%d-%02d-%02d", currentYearMonth.getYear(), currentYearMonth.getMonthValue(), day),
+                    String.format("%02d:%02d", startHour, startMinute),
+                    String.format("%02d:%02d", endHour, endMinute)
+            );
+
+            // Hinzufügen des Termins
+            panelMain.getMonthView().addAppointment(termin);
+        }
     }
 }
