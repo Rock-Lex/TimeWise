fded10c7da8edb7236148a6c50e1c24e90f5eca1
tobi01010010 <rehm.tobias11@gmail.com>
2023-07-29 22:58:07 +0200

    Testklassen kleine änderungen

--- src/Test/TestIntegrations.java
+++ src/Test/TestIntegrations.java
@@ -23,27 +23,27 @@ public class TestIntegrations {
     @Test
     public void testAddTerminWithTeilnehmer() {
 
-        // Create Teilnehmer
+        // Erstelle Teilnehmer
         Teilnehmer teilnehmer = new Teilnehmer("John Doe", "john@email.com");
 
-        // Create TeilnehmerList and add Teilnehmer
+        // Erstelle Termin mit Teilnehmer/////////////////////
         TeilnehmerList teilnehmerList = new TeilnehmerList();
         teilnehmerList.addTeilnehmer(teilnehmer);
 
-        // Create Termin with TeilnehmerList
+        // Erstelle Termin mit Teilnehmer//////////////////////////
         LocalDateTime start = LocalDateTime.of(2023, 2, 15, 9, 0);
         LocalDateTime end = LocalDateTime.of(2023, 2, 15, 10, 0);
         Termin termin = new Termin("Meeting", "Meeting", false, start, end, teilnehmerList);
 
-        // Create TerminListe and add Termin
+        // Erstelle TerminListe und füge Termin hinzu
         TerminListe terminListe = new TerminListe();
         terminListe.addTermin(termin);
 
-        // Verify Termin added successfully
+        // Termin wurde erfolgreich hinzugefügt
         assertEquals(1, terminListe.getTermine().size());
         assertEquals(termin, terminListe.getTermine().get(0));
 
-        // Verify TeilnehmerList added successfully
+        // Teilnehmer wurde erfolgreich zum Termin hinzugefügt
         assertEquals(1, termin.getTeilnehmerList().getTeilnehmerList().size());
         assertTrue(termin.getTeilnehmerList().getTeilnehmerList().contains(teilnehmer));
 
